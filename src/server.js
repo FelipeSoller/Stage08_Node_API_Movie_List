b0fdb2f76c4b6b1d7d8ab7afa5f34ebf41529938
Felipe Soller <soller@outlook.com>
2024-01-11 11:34:04 -0300

    create and config database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const database = require('./database/sqlite')
+const express  = require('express')
+const app      = express()
 
-const app = express()
+database()
 
 const PORT = 3333
 
