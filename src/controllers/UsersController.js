20df3bf844416806d523da329afde1d7efb2cce3
Felipe Soller <soller@outlook.com>
2024-01-12 12:31:03 -0300

    add user update endpoint

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite/')
@@ -19,6 +19,53 @@ class UsersController {
     await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword])
 
     return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, new_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user) {
+      throw new AppError('User not found')
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Email already in use')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(new_password && !password) {
+      throw new AppError('You need to enter your current password to change your password')
+    }
+
+    if(new_password && password) {
+      const checkPassword = await compare(password, user.password)
+
+      if(!checkPassword) {
+        throw new AppError('Your current password is wrong')
+      }
+
+      user.password = await hash(new_password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?
+    `, [user.name, user.email, user.password, id]
+    )
+
+    return response.status(200).json(user)
   }
 }
 
